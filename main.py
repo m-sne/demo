5fd0eb672934fec01b86734a2e2de1e343ab03d8
m-sne <yamad@sfc.keio.ac.jp>
2020-11-17 01:51:52 +0900

    [fix] data.

--- main.py
+++ main.py
@@ -6,18 +6,12 @@ from PIL import Image
 
 def main():
 
-    methods = ['m-sne', 'd-sne', 'dscmr', 'contrastive', 'cross-modal']
-
     dataset = Dataset('./data/deam')
     idx = st.slider('audio number', min_value=0, max_value=100)
     audio = dataset[idx]
-    cols = st.beta_columns(len(methods))
 
-    for i, method in enumerate(methods):
-        with cols[i]:
-            img = Image.open('./data/testers/{}/{}.jpg'.format(method, idx))
-            st.subheader(method)
-            st.image(img, use_column_width=True)
+    img = Image.open('./m-sne/{}.jpg'.format(idx))
+    st.image(img, use_column_width=True)
 
     st.audio(audio)
 
